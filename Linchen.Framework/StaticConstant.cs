8373532ecd2ddd308746af50b3989032fe266d1b
jsyzlinchen <331087108@qq.com>
2019-01-12 12:35:30 +0800

    1

---
+++ Linchen.Framework/StaticConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linchen.Framework
+{
+    public class StaticConstant
+    {
+        public static string SqlServiceConnString = 
+    }
+}
---
+++ Linchen.Libraries.IDAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linchen.Libraries.IDAL
+{
+    public class Class1
+    {
+    }
+}
