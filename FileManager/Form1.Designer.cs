2be8f6b2ed8dbbf21afe98f76b4296b3604868e0
jsyzlinchen <331087108@qq.com>
2019-01-12 12:39:40 +0800

    3 Filemanager

---
+++ FileManager/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tvFolder = new System.Windows.Forms.TreeView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtPath);
+            this.panel1.Controls.Add(this.btnPath);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(989, 96);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(200, 40);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(777, 28);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(26, 33);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(136, 35);
+            this.btnPath.TabIndex = 0;
+            this.btnPath.Text = "选择路径";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tvFolder);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 96);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(185, 490);
+            this.panel2.TabIndex = 1;
+            // 
+            // tvFolder
+            // 
+            this.tvFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvFolder.Location = new System.Drawing.Point(0, 0);
+            this.tvFolder.Name = "tvFolder";
+            this.tvFolder.Size = new System.Drawing.Size(185, 490);
+            this.tvFolder.TabIndex = 0;
+            this.tvFolder.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvFolder_AfterSelect);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lvFiles);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(185, 96);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(804, 490);
+            this.panel3.TabIndex = 2;
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chSize,
+            this.chType,
+            this.chPath});
+            this.lvFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvFiles.Location = new System.Drawing.Point(0, 0);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(804, 490);
+            this.lvFiles.TabIndex = 0;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            this.lvFiles.SelectedIndexChanged += new System.EventHandler(this.lvFiles_SelectedIndexChanged);
+            // 
+            // chName
+            // 
+            this.chName.Text = "文件名称";
+            this.chName.Width = 120;
+            // 
+            // chSize
+            // 
+            this.chSize.Text = "文件大小";
+            this.chSize.Width = 100;
+            // 
+            // chType
+            // 
+            this.chType.Text = "文件类型";
+            this.chType.Width = 79;
+            // 
+            // chPath
+            // 
+            this.chPath.Text = "文件路径";
+            this.chPath.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 586);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "文件管理窗口";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TreeView tvFolder;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chSize;
+        private System.Windows.Forms.ColumnHeader chType;
+        private System.Windows.Forms.ColumnHeader chPath;
+    }
+}
+
---
+++ FileManager/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //选择磁盘目录的 模式窗口对象 
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();//打开模式窗口
+            this.txtPath.Text = fbd.SelectedPath;
+            if (this.txtPath.Text == "") return; //判断没有选择不执行
+
+            TreeNode rootNode = new TreeNode(this.txtPath.Text);//创建根节点名字
+            rootNode.Tag = this.txtPath.Text;//根节点路径
+
+            //在tv里面添加根节点
+            this.tvFolder.Nodes.Add(rootNode);
+            //递归读取文件夹，自己调用自己
+            LoadFolder(rootNode);
+        }
+
+        private TreeNode LoadFolder(TreeNode ChildNode,DirectoryInfo ChildDirectory =null)
+        {
+            //创建好文件夹
+            DirectoryInfo directoryInfo = new DirectoryInfo(ChildNode.Tag.ToString());
+            if (directoryInfo.GetDirectories().Count() <= 0) return null;
+            DirectoryInfo[] list  = directoryInfo.GetDirectories();//获取很多个文件夹
+            foreach (DirectoryInfo item in list)
+            {
+                TreeNode node = new TreeNode(item.Name);//文件夹名
+                node.Tag = item.FullName;//全路径
+                ChildNode.Nodes.Add(node);
+                LoadFolder(node, item);
+            }
+            return null;
+        }
+        /// <summary>
+        /// 选择节点触发事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tvFolder_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            this.lvFiles.Items.Clear();//清楚
+            //获取选中的节点信息
+            TreeNode  selectNode =this.tvFolder.SelectedNode;
+            //查询文件夹里面的文件
+            LoadFiles(selectNode.Tag.ToString());
+
+        }
+        /// <summary>
+        /// 根据文件夹路径读取加载文件信息
+        /// </summary>
+        /// <param name="Path"></param>
+        private void LoadFiles(string path)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            FileInfo[] files = directoryInfo.GetFiles();
+            foreach (var file in files)
+            {
+                ListViewItem item = new ListViewItem(file.Name);
+                item.SubItems.AddRange(new string[] 
+                { (file.Length * 1024).ToString(),file.Extension,  file.FullName});
+                this.lvFiles.Items.Add(item);
+
+            }
+        }
+
+        private void lvFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
