737d0bbac6bcee5d94226cc51d0af227822cdaf7
jsyzlinchen <331087108@qq.com>
2019-03-06 16:26:57 +0800

    123

--- Linchen.Libraries.IDAL/IBaseDAL.cs
+++ Linchen.Libraries.IDAL/IBaseDAL.cs
@@ -7,27 +7,13 @@ namespace Linchen.Libraries.IDAL
     public interface IBaseDAL
     {
 
-        // 约束是为了正常的调用
-        // 能保证传的T 是BaseModel的子类
-        //1,            
-        //GetColumnName 字段名  Properties特性   列名columnString
         T Find<T>(int id) where T : BaseModel;
 
-
-        //2.
         List<T> FindAll<T>() where T : BaseModel;
-
-
-        //优化封装
-        //3.
-        //List<T> ReadToList<T>(SqlDataReader reader);
-
 
         void Update<T>(T t) where T : BaseModel;
 
-
         void Insert<T>(T t) where T : BaseModel;
-
 
         void Delete<T>(int id) where T : BaseModel;
        
